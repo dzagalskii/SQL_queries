973c67d93cedcaff071e0e75f04a7475e895350b
zagalskij <dzagalskij@mail.ru>
2021-11-02 17:06:37 +0300

    - начал делать страницу с контрольными

--- queries/views.py
+++ queries/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from dbserver.models import AST
 
@@ -5,9 +6,12 @@ from dbserver.models import AST
 
 
 def all_queries(request):
-    all_ast_queries = AST.objects.all()
-    all_ast_sec_queries = all_ast_queries.values('db_index', 'db_request')
-    # for ast_query in all_ast_queries:
-    #     print("{} {} {}".format(ast_query.db_index, ast_query.db_request, ast_query.db_answer))
-    print(all_ast_sec_queries)
-    return render(request, 'all_queries.html', {'all_ast_sec_queries': all_ast_sec_queries})
+    if request.user.is_authenticated:
+        all_ast_queries = AST.objects.all()
+        all_ast_sec_queries = all_ast_queries.values('db_index', 'db_request')
+        # for ast_query in all_ast_queries:
+        #     print("{} {} {}".format(ast_query.db_index, ast_query.db_request, ast_query.db_answer))
+        print(all_ast_sec_queries)
+        return render(request, 'all_queries.html', {'all_ast_sec_queries': all_ast_sec_queries})
+    else:
+        return HttpResponseRedirect('/account/login/')
