172175dce713fd58dc4849eb18fb5053adf5a913
zagalskij <dzagalskij@mail.ru>
2021-11-04 12:31:39 +0300

    - TODO сделать шаблон для письма активации аккаунта

--- account/urls.py
+++ account/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('register/', views.signup, name="register"),
     path('activate/<uidb64>/<token>/', views.activate, name="activate"),
     path('password_reset/',
-         PasswordResetView.as_view(template_name='registration/password_reset_form_.html',),
+         PasswordResetView.as_view(template_name='registration/password_reset_form_.html', ),
          name='password_reset',
          ),
     path('password_reset_done/',
@@ -25,4 +25,16 @@ urlpatterns = [
          name='password_reset_complete')
 ]
 
-#html_email_template_name='registration/acc_active_email.html'
+# TODO: в password_reset надо заменить стандартный шаблон на такой же, как 'registration/acc_active_email.html'
+# Скопировать шаблон 'registration/acc_active_email.html', переименовать и отредачить для подтверждение регистрации:
+# 1. чтобы туда правильно подставилась ссылка в нужное место
+# 2. текст в нем немного переделать, чтобы подтверждению регистрации соответствовал
+
+# Есть вот такой варик замены шаблона, он действительно работает, но как подставить в шаблон значения необходимые,
+# я не понял
+'''
+path('password_reset/',
+     PasswordResetView.as_view(template_name='registration/password_reset_form_.html',
+     html_email_template_name='registration/acc_active_email.html'),
+     name='password_reset',),
+'''
