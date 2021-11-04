f00e1b311df29250668d5b5dc6bff422f71c8a32
Nonoleg17 <Nonoleg17@mail.ru>
2021-11-04 12:36:55 +0300

    Add MsSQL

--- dbserver/Core/DBConnect.py
+++ dbserver/Core/DBConnect.py
@@ -1,6 +1,9 @@
 import psycopg2
+import pyodbc
 from .SqlRequestCheck import *
 from dbserver.models import AST
+
+
 
 
 def connect_to_postgres():
@@ -19,6 +22,32 @@ def connect_to_postgres():
         print(rows)
         cur.execute(
             'SELECT ast1.Code  FROM ASt as ast1 inner join ASt as ast2 on ast1.UPSys = ast2.Code  WHERE ast1.Cost_pm <= ast2.Cost_pm;')
+        new_rows = cur.fetchall()
+        print(new_rows)
+        if rows == new_rows:
+            print('ok')
+        else:
+            find_error(rows, new_rows)
+
+    except Exception:
+        print(Exception)
+
+    con.close()
+
+
+def connect_to_MsSQL():
+    sql_request = AST.objects.get(db_index='CQ-2')
+    con = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                         "Server=DESKTOP-J2U6QDF;"
+                         "Database=master;"
+                         "Trusted_Connection=yes;")
+    cur = con.cursor()
+    try:
+        cur.execute(sql_request.db_answer)
+        rows = cur.fetchall()
+        print(rows)
+        cur.execute(
+            'SELECT ast1.Name  FROM ASt as ast1 inner join ASt as ast2 on ast1.UPSys = ast2.Code  WHERE ast1.Cost_pm <= ast2.Cost_pm;')
         new_rows = cur.fetchall()
         print(new_rows)
         if rows == new_rows:
--- dbserver/views.py
+++ dbserver/views.py
@@ -4,7 +4,7 @@ from .Core.DBConnect import *
 
 
 def index(request):
-    connect_to_postgres()
+    connect_to_MsSQL()
     # print(sql_request.db_answer)
 
     return HttpResponse("Hello, world. You're at the polls index.")
