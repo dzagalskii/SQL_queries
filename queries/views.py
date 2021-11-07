bf7f552c79d7511210249dac7eef383dd006bbc3
zagalskij <dzagalskij@mail.ru>
2021-11-07 17:50:51 +0300

    - сделал страницу с контрольной работой

--- queries/views.py
+++ queries/views.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from dbserver.models import AST
@@ -9,11 +11,6 @@ from dbserver.models import AST
 def all_control_works(request):
     if request.user.is_authenticated:
         all_user_schemas = request.user.data_schemas.split()
-        all_ast_queries = AST.objects.all()
-        all_ast_sec_queries = all_ast_queries.values('db_index', 'db_request')
-        # for ast_query in all_ast_queries:
-        #     print("{} {} {}".format(ast_query.db_index, ast_query.db_request, ast_query.db_answer))
-        print(all_ast_sec_queries)
         return render(request, 'all_control_works.html',
                       {'img_1': 'img/' + all_user_schemas[0] + '.jpg',
                        'img_2': 'img/' + all_user_schemas[1] + '.jpg',
@@ -29,6 +26,30 @@ def control_work(request, control_work_id):
             return HttpResponseRedirect('/queries')
         all_user_schemas = request.user.data_schemas.split()
         schema = all_user_schemas[control_work_id - 1]
-        print(schema)
+
+        # if schema == 'AST':
+        #     all_queries = AST.objects.all()
+        # elif schema == 'SPJ':
+        #     all_queries = SPJ.objects.all()
+        # elif schema == 'WF':
+        #     all_queries = WF.objects.all()
+
+        all_queries = AST.objects.all()
+        all_sec_queries = all_queries.values('db_index', 'db_request')
+
+        random_three_queries = []
+        for i in range(3):
+            random_three_queries.append(all_sec_queries[randint(0, len(all_sec_queries) - 1)])
+        print(random_three_queries)
+
+        return render(request, 'control_work.html',
+                      {'control_work_id': control_work_id,
+                       'img': 'img/' + all_user_schemas[control_work_id - 1] + '.jpg',
+                       'random_three_queries': random_three_queries})
+
+        # all_ast_sec_queries = all_queries.values('db_index', 'db_request')
+        # for ast_query in all_ast_queries:
+        #     print("{} {} {}".format(ast_query.db_index, ast_query.db_request, ast_query.db_answer))
+        # print(all_ast_sec_queries)
     else:
         return HttpResponseRedirect('/account/login/')
