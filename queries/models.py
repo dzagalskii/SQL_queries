89c36d2c20b6c6b77501736bcc2887998eff7f89
zagalskij <dzagalskij@mail.ru>
2021-11-13 16:09:52 +0300

    - убрал модель контрольной работы

--- queries/models.py
+++ queries/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 from django.conf import settings
 
-
-class ControlWork(models.Model):
-    number = models.IntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    first_query_answer = models.CharField(max_length=1000)
-    second_query_answer = models.CharField(max_length=1000)
-    third_query_answer = models.CharField(max_length=1000)
-
