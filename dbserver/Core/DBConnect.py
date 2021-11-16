915215d09f553c2b72447c047d3c67b0b4b917c2
dzagalskii <dzagalskij@mail.ru>
2021-11-16 09:57:41 +0300

    - подобие обработки ошибок

--- dbserver/Core/DBConnect.py
+++ dbserver/Core/DBConnect.py
@@ -6,18 +6,24 @@ from .SqlRequestCheck import *
 def check_query(reference_code, user_code, database):
     # TODO: ПЕРЕНЕСТИ ПАРАМЕТРЫ В SETTINGS
     if database == "PG":
-        con = psycopg2.connect(
-            database="bobsdb",
-            user="postgres",
-            password="0512",
-            host="127.0.0.1",
-            port="5432"
-        )
+        try:
+            con = psycopg2.connect(
+                database="bobsdb",
+                user="postgres",
+                password="0512",
+                host="127.0.0.1",
+                port="5432"
+            )
+        except:
+            return None, "Error with DB connection (PG)"
     elif database == "MS":
-        con = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                             "Server=DESKTOP-J2U6QDF;"
-                             "Database=master;"
-                             "Trusted_Connection=yes;")
+        try:
+            con = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                                 "Server=DESKTOP-J2U6QDF;"
+                                 "Database=master;"
+                                 "Trusted_Connection=yes;")
+        except:
+            return None, "Error with DB connection"
     else:
         return None, "Unknown database"
     cur = con.cursor()
@@ -36,4 +42,3 @@ def check_query(reference_code, user_code, database):
             return find_error(reference_output, reference_output), None
     except Exception:
         return None, "Exception"
-
