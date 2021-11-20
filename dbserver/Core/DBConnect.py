c32dfcc202fef3f549740bdf0b6de4af62d82a44
Nonoleg17 <Nonoleg17@mail.ru>
2021-11-20 16:57:31 +0300

    Hz

--- dbserver/Core/DBConnect.py
+++ dbserver/Core/DBConnect.py
@@ -3,19 +3,28 @@ import pyodbc
 from .SqlRequestCheck import *
 
 
+def filter_query(user_code):
+    restricted_words = ['insert', 'delete', 'update', 'create', 'use', 'show', 'source', 'drop', 'describe']
+    user_code = user_code.lower()
+    for i in restricted_words:
+        if (" " + i + " ") in (" " + user_code + " "):
+            return -1
+    return 0
+
+
 def check_query(reference_code, user_code, database):
     # TODO: ПЕРЕНЕСТИ ПАРАМЕТРЫ В SETTINGS
+    if filter_query(user_code) == -1:
+        return None, "Недопустимый запрос"
+
     if database == "PG":
-        try:
-            con = psycopg2.connect(
-                database="bobsdb",
-                user="postgres",
-                password="0512",
-                host="127.0.0.1",
-                port="5432"
-            )
-        except:
-            return None, "Error with DB connection (PG)"
+        con = psycopg2.connect(
+            database="BoBSDB",
+            user="postgres",
+            password="admin",
+            host="127.0.0.1",
+            port="5432"
+        )
     elif database == "MS":
         try:
             con = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
@@ -39,6 +48,7 @@ def check_query(reference_code, user_code, database):
         if reference_output == user_output:
             return "OK", None
         else:
-            return find_error(reference_output, reference_output), None
+            return find_error(reference_output, user_output), None
     except Exception:
+        con.close()
         return None, "Exception"
--- dbserver/Core/SqlRequestCheck.py
+++ dbserver/Core/SqlRequestCheck.py
@@ -7,11 +7,27 @@ def check_len_of_rows(len_row, len_new_row):
         return 0
 
 
+def check_null(row, new_row):
+    row_flag, new_row_flag = False, False
+    for i in row:
+        if None in i:
+            row_flag = True
+    for i in new_row:
+        if None in i:
+            new_row_flag = True
+    if row_flag != new_row_flag:
+        return -1
+    return 0
+
+
 def find_error(rows, new_rows):
+    check_null_res = check_null(rows, new_rows)
+    if check_null_res == -1:
+        return 'Проблема с NULL'
     check_len_res = check_len_of_rows(len(rows), len(new_rows))
     if check_len_res == 1:
-        return 'Not enough data'
+        return 'Недостаточно данных'
     elif check_len_res == -1:
-        return 'Too much data'
+        return 'Слишком много данных'
     else:
-        return 'Unknown error'
+        return 'Неверный результат'
