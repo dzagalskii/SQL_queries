792f85c0648ea9559a416f4cf257bf715e848c57
zagalskij <dzagalskij@mail.ru>
2022-03-06 00:16:46 +0300

    добавил docker-compose.yml

--- dbserver/Core/DBConnect.py
+++ dbserver/Core/DBConnect.py
@@ -30,7 +30,7 @@ def check_query(reference_code, user_code, database):
             return None, "Error with DB connection"
     elif database == "MS":
         try:
-            con = pyodbc.connect("DRIVER={SQLDocker};"
+            con = pyodbc.connect("DRIVER={FreeTDS};"
                                  "SERVER=mssql_container;"
                                  "PORT=1433;"
                                  "DATABASE=msdb;"
