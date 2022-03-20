49d0e3ccd85b1a5add616e8cf2e9fe8a13d6bf93
dzaga <dzagalskij@mail.ru>
2022-03-20 21:38:46 +0300

    убрал go

--- config/ms/init_db.sql
+++ config/ms/init_db.sql
@@ -292,3 +292,13 @@ create table Fr
     Bugjet int
 );
 
+create table Works
+(
+    W      int,
+    F      int,
+    Cost   int,
+    Status varchar(15),
+    PRIMARY KEY (W, F),
+    CONSTRAINT FK_ES FOREIGN KEY (F) REFERENCES Fr (Code),
+    CONSTRAINT FK_SE FOREIGN KEY (W) REFERENCES Wr (Code)
+);
